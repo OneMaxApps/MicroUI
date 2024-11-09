124927f1ef1cd8c839f63ec4ec698d3e0aeefbeb
002 <002@192.168.42.44>
2024-11-10 02:04:19 +0300

    checking for error "zero by" in eclipse

--- MicroUI/src/microUI/test/Main.java
+++ MicroUI/src/microUI/test/Main.java
@@ -18,11 +18,7 @@ public class Main extends PApplet {
 	
 	public void setup() {
 		grid = new GridLayout(this,3);
-		grid.margin.set(10);
-		grid.add(new Button(this), 0,0);
-		grid.add(new EdgeLayout(this).set(new Button(this,"Button inside EdgeLayout",0,0,100,50)), 1,0);
-		grid.add(new Slider(this), 2,0);
-		grid.add(new Scroll(this), 1,1);
+		
 	}
 	
 	public void draw() {
