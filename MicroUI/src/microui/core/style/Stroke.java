7d8ac09f7f75b9522cb86359cd0907f862b03296
OneMaxApps <139242637+OneMaxApps@users.noreply.github.com>
2025-09-13 14:55:34 +0300

    fix(Stroke): now it's more safer

--- MicroUI/src/microui/core/style/Stroke.java
+++ MicroUI/src/microui/core/style/Stroke.java
@@ -1,40 +1,55 @@
 package microui.core.style;
 
-import static processing.core.PConstants.SQUARE;
+import static microui.MicroUI.getContext;
 
-import microui.MicroUI;
 import microui.util.Metrics;
 
 public final class Stroke {
+	private static final int SQUARE = 1;
 	private final Color color;
-    private int weight;
-    
-    public Stroke() {
-      Metrics.register("Stroke");
-      
-      color = new Color(0);
-      weight = 2;
-    }
-    
-    public void apply() {
-      MicroUI.getContext().strokeCap(SQUARE);
-      MicroUI.getContext().stroke(color.get());
-      MicroUI.getContext().strokeWeight(weight);
-    }
-    
-    public final void setWeight(final int size) { this.weight = size; }
-    public final int getWeight() { return weight; }
-    
-    public final void set(final Stroke stroke) {
-    	color.set(stroke.color);
-    	weight = stroke.getWeight();
-    }
-    
-	public final Color getColor() {
+	private int weight;
+
+	public Stroke() {
+		Metrics.register("Stroke");
+
+		color = new Color(0);
+		weight = 2;
+	}
+
+	public void apply() {
+		getContext().strokeCap(SQUARE);
+		getContext().stroke(color.get());
+		getContext().strokeWeight(weight);
+	}
+
+	public void setWeight(final int size) {
+		if(weight <= 0) {
+			throw new IllegalArgumentException("stroke weight cannot be lower than 1");
+		}
+		this.weight = size;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void set(Stroke stroke) {
+		if(stroke == null) {
+			throw new NullPointerException("stroke cannot be null");
+		}
+		
+		color.set(stroke.color);
+		weight = stroke.getWeight();
+	}
+
+	public Color getColor() {
 		return new Color(color);
 	}
-	
-	public final void setColor(Color color) {
+
+	public void setColor(Color color) {
+		if(color == null) {
+			throw new NullPointerException("color cannot be null");
+		}
 		this.color.set(color);
 	}
 }
