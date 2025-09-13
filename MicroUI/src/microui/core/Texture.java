b2951509e0cf15530f535c5a27d6f39c412ba68b
OneMaxApps <139242637+OneMaxApps@users.noreply.github.com>
2025-09-13 15:28:15 +0300

    fix(Texture): now he is safer for illegal dimensions

--- MicroUI/src/microui/core/Texture.java
+++ MicroUI/src/microui/core/Texture.java
@@ -1,5 +1,6 @@
 package microui.core;
 
+import static java.lang.Math.max;
 import static java.util.Objects.requireNonNull;
 
 import microui.core.base.SpatialView;
@@ -11,42 +12,43 @@ import processing.core.PImage;
 public class Texture extends SpatialView {
 	private final Color color;
 	private PImage image;
-	
+
 	public Texture() {
 		setVisible(true);
 		color = new Color(255);
-		
 	}
-	
+
 	@Override
 	public void draw() {
-		if(isLoaded()) { super.draw(); }
+		if (isLoaded()) {
+			super.draw();
+		}
 	}
-	
+
 	@Override
 	protected void render() {
-		if(isLoaded()) {
-		  ctx.pushStyle();
-		  ctx.tint(color.get());
-		  ctx.image(image,getX(), getY(), getWidth(), getHeight());
-		  ctx.popStyle();
+		if (isLoaded()) {
+			ctx.pushStyle();
+			ctx.tint(color.get());
+			ctx.image(image, getX(), getY());
+			ctx.popStyle();
 		}
 	}
 
 	public final boolean isLoaded() {
 		return image != null;
 	}
-	
+
 	public final void set(final PImage image) {
 		this.image = requireNonNull(image, "image cannot be null");
-		this.image.resize((int) getWidth(), (int) getHeight());
+		updateDimensionsOfImageCorrect(this);
 	}
-	
+
 	public final void load(final String path) {
 		image = ctx.loadImage(requireNonNull(path, "path cannot be null"));
-		image.resize((int) getWidth(), (int) getHeight());
+		updateDimensionsOfImageCorrect(this);
 	}
-	
+
 	public final PImage get() {
 		return image;
 	}
@@ -56,13 +58,17 @@ public class Texture extends SpatialView {
 	}
 
 	public final void setColor(Color color) {
-		this.color.set(requireNonNull(color,"color cannot be null"));
+		this.color.set(requireNonNull(color, "color cannot be null"));
 	}
 
 	@Override
 	protected void onChangeDimensions() {
 		super.onChangeDimensions();
-		if(image == null) { return; }
-		image.resize((int) getWidth(), (int) getHeight());
+		updateDimensionsOfImageCorrect(this);
+	}
+
+	private static final void updateDimensionsOfImageCorrect(Texture texture) {
+		if(texture == null || texture.get() == null) { return; }
+		texture.get().resize((int) max(1,texture.getWidth()) , (int) max(1,(texture.getHeight()));
 	}
 }
